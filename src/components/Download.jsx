302fb51ebe35c3ca50502c0b42a1937af808c4a3
gixi <104705119+gioliotta@users.noreply.github.com>
2023-11-18 19:51:22 -0800

    change link exe

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -1,31 +1,12 @@
-// import donwloadIcon from "../assets/download-icon.svg";
-
 function Download() {
-  // function downloadFile() {
-  //   const link = document.createElement("a");
-  //   link.href = "../../LymWrite-setup.exe";
-  //   link.download = "LymWrite-setup.exe";
-  //   document.body.appendChild(link);
-  //   link.click();
-  //   document.body.removeChild(link);
-  // }
-
   return (
     <div className="container-download">
       <p className="download-text">
-        <a download href="../../LymWrite-setup.exe">
+        <a download href="https://lym-write.netlify.app/LymWrite-setup.exe">
           Descargar para Windows
         </a>
         Versión: 1.0.0 &nbsp;&nbsp;&nbsp;Tamaño: 11.45 MB
       </p>
-
-      {/* <img
-        onClick={downloadFile}
-        style={{ cursor: "pointer" }}
-        src={donwloadIcon}
-        title="Descargar exe"
-        alt="Descargar Icono"
-      /> */}
     </div>
   );
 }
