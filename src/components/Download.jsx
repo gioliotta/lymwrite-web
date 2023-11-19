15b4f80de265f45a07465fda9178df391475a9da
gixi <104705119+gioliotta@users.noreply.github.com>
2023-11-18 20:00:47 -0800

    change link exe

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -2,7 +2,7 @@ function Download() {
   return (
     <div className="container-download">
       <p className="download-text">
-        <a download href="https://lym-write.netlify.app/LymWrite-setup.exe">
+        <a download href="./LymWrite-setup.exe">
           Descargar para Windows
         </a>
         Versión: 1.0.0 &nbsp;&nbsp;&nbsp;Tamaño: 11.45 MB
