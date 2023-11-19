4bfff9d1751ab0dd00d7554ee4998a35ab524cc0
gixi <104705119+gioliotta@users.noreply.github.com>
2023-11-18 20:58:31 -0800

    link provisional de descarga

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -2,7 +2,10 @@ function Download() {
   return (
     <div className="container-download">
       <p className="download-text">
-        <a download href="./LymWrite-setup.exe">
+        <a
+          download
+          href="https://drive.google.com/file/d/13LxIzKvQXfckMr1yNJFFeMN2NEfFdpUF"
+        >
           Descargar para Windows
         </a>
         Versión: 1.0.0 &nbsp;&nbsp;&nbsp;Tamaño: 11.45 MB
